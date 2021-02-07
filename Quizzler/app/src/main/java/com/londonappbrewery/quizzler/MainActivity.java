552b9bb9d65f5c37ea062fb393a4e061802b9d5c
Shahzad-Adil <68489493+Shahzad-Adil@users.noreply.github.com>
2021-02-07 21:56:26 +0500

    add comment

--- Quizzler/app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ Quizzler/app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -105,6 +105,7 @@ public class MainActivity extends Activity {
     public void falseBtnListener(View view) {
         checkFalseAnswer();
         updateQuestion();
+        //comment
     }
 
     private void checkFalseAnswer() {
